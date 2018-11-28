8a9c2fa6aca8693b3179a5f6ccbefab9bc0d9a19
Chris <chris.kan@quantatw.com>
2018-11-28 11:01:22 +0800

    [Lesson 4] Decision Trees mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,7 +25,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train,labels_train,)
+pred = clf.predict(features_test)
 
+accury = accuracy_score(pred,labels_test)
+print "DT tree accuracy: %r" % (accury)
+
+print "number of features: %r" % (len(features_train[0]))
+print features_train.shape
 
 #########################################################
 
